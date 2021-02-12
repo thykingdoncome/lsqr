0ab3c064c892dd9c221473e5a8ca4053cc266555
israel david <davidisrael194@gmail.com>
2021-02-12 23:36:28 +0100

    usr scrrev

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import InfoCard from '../components/Cards/InfoCard'
-import PageTitle from '../components/Typography/PageTitle'
-import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from '../icons'
-import RoundIcon from '../components/RoundIcon'
-import response from '../utils/demo/usersData'
-import { Tag } from 'antd'
-import { MdMoreVert } from 'react-icons/md'
+import React, { useState, useEffect } from "react";
+import InfoCard from "../components/Cards/InfoCard";
+import PageTitle from "../components/Typography/PageTitle";
+import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from "../icons";
+import RoundIcon from "../components/RoundIcon";
+import response from "../utils/demo/usersData";
+import { Tag } from "antd";
+import { MdMoreVert } from "react-icons/md";
+import { BsFilter } from "react-icons/bs";
+
 import {
   TableBody,
   TableContainer,
@@ -15,30 +17,31 @@ import {
   TableRow,
   TableFooter,
   Pagination,
-} from '@windmill/react-ui'
+} from "@windmill/react-ui";
 
+import { formatAmount } from "../utils/funcs/FormatAmount";
+import { formatDate } from "../utils/funcs/FormatDate";
 
-function Users({history }) {
-
-  const [page, setPage] = useState(1)
-  const [data, setData] = useState([])
+function Users({ history }) {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
 
   // pagination setup
-  const resultsPerPage = 9
-  const totalResults = response.length
+  const resultsPerPage = 9;
+  const totalResults = response.length;
 
   // pagination change control
   function onPageChange(p) {
-    setPage(p)
+    setPage(p);
   }
 
   // on page change, load new sliced data
   // here you would make another server request for new data
   useEffect(() => {
-    setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage))
-  }, [page])
+    setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage));
+  }, [page]);
 
-  const handleViewUser = (e) => history.push(`/app/user-profile/${e}`)
+  const handleViewUser = (e) => history.push(`/app/user-profile/${e}`);
 
   return (
     <>
@@ -46,7 +49,7 @@ function Users({history }) {
 
       {/* <!-- Cards --> */}
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-      <InfoCard title="USERS" value="6,389">
+        <InfoCard title="USERS" value={formatAmount(6389)}>
           <RoundIcon
             icon={PeopleIcon}
             iconColorClass="text-orange-500 dark:text-orange-100"
@@ -55,7 +58,7 @@ function Users({history }) {
           />
         </InfoCard>
 
-        <InfoCard title="ACTIVE USERS" value="5,089">
+        <InfoCard title="ACTIVE USERS" value={formatAmount(6389)}>
           <RoundIcon
             icon={MoneyIcon}
             iconColorClass="text-green-500 dark:text-green-100"
@@ -64,7 +67,7 @@ function Users({history }) {
           />
         </InfoCard>
 
-        <InfoCard title="USERS WITH LOANS" value="3,766">
+        <InfoCard title="USERS WITH LOANS" value={formatAmount(16389)}>
           <RoundIcon
             icon={CartIcon}
             iconColorClass="text-blue-500 dark:text-blue-100"
@@ -73,7 +76,7 @@ function Users({history }) {
           />
         </InfoCard>
 
-        <InfoCard title="USERS WITH SAVINGS" value="5,005">
+        <InfoCard title="USERS WITH SAVINGS" value={formatAmount(106389)}>
           <RoundIcon
             icon={ChatIcon}
             iconColorClass="text-teal-500 dark:text-teal-100"
@@ -83,68 +86,126 @@ function Users({history }) {
         </InfoCard>
       </div>
 
-        <TableContainer className='bg-white p-8 table-container'>
-          <Table>
-            <TableHeader>
-              <tr>
-                <TableCell>ORGANIZATIONS</TableCell>
-                <TableCell>USERNAME</TableCell>
-                <TableCell>EMAIL</TableCell>
-                <TableCell>PHONE NUMBER</TableCell>
-                <TableCell>DATE JOINED</TableCell>
-                <TableCell>STATUS</TableCell>
-                <TableCell></TableCell>
-              </tr>
-            </TableHeader>
-            <TableBody>
-              {data.map((user) => (
-                <TableRow key={user.id} onClick={() => handleViewUser(user.username)}>
-                  <TableCell>
-                        <p className="font-semibold">{user.organization}</p>
-                  </TableCell>
-                  <TableCell>
-                    <p>{user.username}</p>
-                  </TableCell>
-                  <TableCell>
-                    <p>{user.email}</p>
-                  </TableCell>
-                  <TableCell>
-                    <p>{user.telephone}</p>
-                  </TableCell>
-                  <TableCell>
-                    <p>{new Date(user.dateJoined).toLocaleDateString()}</p>
-                  </TableCell>
+      <TableContainer className="bg-white p-8 table-container">
+        <Table>
+          <TableHeader className="text-pale-purple text-xs">
+            <tr>
+              <TableCell>
+                <div className="flex items-center">
+                  <span>ORGANIZATIONS</span>{" "}
+                  <span className='pl-2'>
+                    <BsFilter />
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center">
+                  <span>USERNAME</span>{" "}
+                  <span className='pl-2'>
+                    <BsFilter />
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center">
+                  <span>EMAIL</span>{" "}
+                  <span className='pl-2'>
+                    <BsFilter />
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center">
+                  <span>PHONE NUMBER</span>{" "}
+                  <span className='pl-2'>
+                    <BsFilter />
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center">
+                  <span>DATE JOINED</span>{" "}
+                  <span className='pl-2'>
+                    <BsFilter />
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center">
+                  <span>STATUS</span>{" "}
+                  <span className='pl-2'>
+                    <BsFilter />
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell></TableCell>
+            </tr>
+          </TableHeader>
+          <TableBody>
+            {data.map((user) => (
+              <TableRow
+                key={user.id}
+                onClick={() => handleViewUser(user.username)}
+                className="text-pale-purple text-sm cursor-pointer"
+              >
+                <TableCell className="align-middle">
+                  <span>{user.organization}</span>
+                </TableCell>
+                <TableCell className="align-middle">
+                  <span>{user.username}</span>
+                </TableCell>
+                <TableCell className="align-middle">
+                  <span>{user.email}</span>
+                </TableCell>
+                <TableCell className="align-middle">
+                  <span>{user.telephone}</span>
+                </TableCell>
+                <TableCell className="align-middle">
+                  <span>{formatDate(user.dateJoined)}</span>
+                </TableCell>
 
-                  <TableCell>
-                    <Tag 
-                    style={{padding: '.2rem', width: '100%', textAlign: 'center', borderRadius: '20px'}}
-                     color={  user.status === 'Blacklisted' ? 'red' : 
-                        user.status === 'Active' ? 'green' :
-                        user.status === 'Inactive' ? '#bcc2d2' : 'gold' }>
-                      {user.status}
-                    </Tag>
-                  </TableCell>
+                <TableCell className="align-middle">
+                  <Tag
+                    style={{
+                      padding: ".2rem",
+                      width: "100%",
+                      textAlign: "center",
+                      borderRadius: "20px",
+                    }}
+                    color={
+                      user.status === "Blacklisted"
+                        ? "red"
+                        : user.status === "Active"
+                        ? "green"
+                        : user.status === "Inactive"
+                        ? "#bcc2d2"
+                        : "gold"
+                    }
+                  >
+                    {user.status}
+                  </Tag>
+                </TableCell>
 
-                  <TableCell>
-                      <MdMoreVert />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-          <TableFooter>
-          <Pagination
-            totalResults={totalResults}
-            resultsPerPage={resultsPerPage}
-            onChange={onPageChange}
-            label="Table navigation"
-          />
-        </TableFooter>
-
-
+                <TableCell>
+                  <button className="z-50 cursor-default">
+                    <MdMoreVert onClick={() => console.log(user, "userrr")} />
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableFooter>
+        <Pagination
+          totalResults={totalResults}
+          resultsPerPage={resultsPerPage}
+          onChange={onPageChange}
+          label="Table navigation"
+        />
+      </TableFooter>
     </>
-  )
+  );
 }
 
-export default Users
+export default Users;
