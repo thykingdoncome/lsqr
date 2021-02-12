6af6a0c6eda2c695bca3769d9e28956b6839ee52
israel david <davidisrael194@gmail.com>
2021-02-12 16:04:30 +0100

    ***

--- src/components/Cards/InfoCard.js
+++ src/components/Cards/InfoCard.js
@@ -8,7 +8,7 @@ function InfoCard({ title, value, children: icon }) {
         {icon}
         <div>
           <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">{title}</p>
-          <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">{value}</p>
+          <p className="text-lg font-semibold text-gray-700" >{value}</p>
         </div>
       </CardBody>
     </Card>
